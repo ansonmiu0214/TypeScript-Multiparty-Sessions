d5af4f299575817fd87aac7a8a8fb518f1f1a467
Anson Miu <ansonmiu0214@gmail.com>
2020-04-09 17:33:34 +0800

    Minor update for Node EFSM template

--- mpst_ts/codegen/generator/node/__init__.py
+++ mpst_ts/codegen/generator/node/__init__.py
@@ -1,2 +1,2 @@
-from .node_v2.node_strategy import NodeStrategy
-# from .node_v3.node_strategy import NodeStrategy
+# from .node_v2.node_strategy import NodeStrategy
+from .node_v3.node_strategy import NodeStrategy
