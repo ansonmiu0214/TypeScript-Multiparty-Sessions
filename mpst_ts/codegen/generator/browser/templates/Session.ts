fbca3248acc9031f29736c0499833166e79d6d2a
Anson Miu <ansonmiu0214@gmail.com>
2020-04-13 23:39:58 +0800

    Fix type signature for React message sending event handler

--- mpst_ts/codegen/generator/browser/templates/Session.ts
+++ mpst_ts/codegen/generator/browser/templates/Session.ts
@@ -17,8 +17,17 @@ export function isTerminalState(state: State): state is TerminalState {
   return (Object.values(TerminalState) as Array<State>).includes(state)
 }
 
+type FunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends Function | undefined ? K : never;
+}[keyof T];
+type FunctionProperties<T> = Pick<T, FunctionPropertyNames<T>>;
+
+export type DOMEvents = FunctionProperties<React.DOMAttributes<any>>
+
+export type FunctionArguments<T> = T extends (...args: infer R) => infer _ ? R : never;
+
 export type Constructor<T> = new (...args: any[]) => T;
-export type EventHandler<Payload> = (event: UIEvent) => Payload;
-export type SendComponentFactory<Payload> = (event: string, handler: EventHandler<Payload>) => Constructor<Component>;
+export type EventHandler<Payload, K extends keyof DOMEvents> = (event: FunctionArguments<DOMEvents[K]>) => Payload;
+export type SendComponentFactory<Payload> = <K extends keyof DOMEvents> (event: K, handler: EventHandler<Payload, K>) => Constructor<Component>;
 export type SendComponentFactoryFactory = <Payload> (label: string, successor: State) => SendComponentFactory<Payload>;
 export type ReceiveHandler = (message: any) => State;
