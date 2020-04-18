f4674277d3440f4b7fab6529ef7178fda81e31e8
Anson Miu <ansonmiu0214@gmail.com>
2020-04-18 19:22:33 +0800

    Include custom type imports in templates

--- mpst_ts/codegen/generator/browser/browser_v2/templates/Session.ts
+++ mpst_ts/codegen/generator/browser/browser_v2/templates/Session.ts
@@ -1,9 +1,5 @@
-import { Component } from 'react';
+import React from 'react';
 import { State, ReceiveState, SendState, TerminalState } from './EFSM';
-
-export interface IReceive {
-  handle(message: any): State
-}
 
 export function isReceiveState(state: State): state is ReceiveState {
   return (Object.values(ReceiveState) as Array<State>).includes(state)
@@ -28,6 +24,6 @@ export type FunctionArguments<T> = T extends (...args: infer R) => infer _ ? R :
 
 export type Constructor<T> = new (...args: any[]) => T;
 export type EventHandler<Payload, K extends keyof DOMEvents> = (event: FunctionArguments<DOMEvents[K]>) => Payload;
-export type SendComponentFactory<Payload> = <K extends keyof DOMEvents> (event: K, handler: EventHandler<Payload, K>) => Constructor<Component>;
+export type SendComponentFactory<Payload> = <K extends keyof DOMEvents> (event: K, handler: EventHandler<Payload, K>) => Constructor<React.Component>;
 export type SendComponentFactoryFactory = <Payload> (label: string, successor: State) => SendComponentFactory<Payload>;
 export type ReceiveHandler = (message: any) => State;
