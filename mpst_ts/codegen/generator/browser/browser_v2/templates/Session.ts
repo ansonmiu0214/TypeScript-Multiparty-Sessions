0b1977600579dc13cb14f76afdd186a3e0b6c8fb
Anson Miu <ansonmiu0214@gmail.com>
2020-05-02 15:55:19 +0800

    Extend async support for frontend send/receive implementations

--- mpst_ts/codegen/generator/browser/browser_v2/templates/Session.ts
+++ mpst_ts/codegen/generator/browser/browser_v2/templates/Session.ts
@@ -23,7 +23,10 @@ export type DOMEvents = FunctionProperties<React.DOMAttributes<any>>
 export type FunctionArguments<T> = T extends (...args: infer R) => infer _ ? R : never;
 
 export type Constructor<T> = new (...args: any[]) => T;
-export type EventHandler<Payload, K extends keyof DOMEvents> = (event: FunctionArguments<DOMEvents[K]>) => Payload;
+export type EventHandler<Payload, K extends keyof DOMEvents> = (event: FunctionArguments<DOMEvents[K]>) => MaybePromise<Payload>;
 export type SendComponentFactory<Payload> = <K extends keyof DOMEvents> (event: K, handler: EventHandler<Payload, K>) => Constructor<React.Component>;
 export type SendComponentFactoryFactory = <Payload> (role: Roles, label: string, successor: State) => SendComponentFactory<Payload>;
-export type ReceiveHandler = (message: any) => State;
+export type ReceiveHandler = (message: any) => MaybePromise<State>;
+
+export type MaybePromise<T> = T | Promise<T>;
+export type FromJust<T> = T extends MaybePromise<infer R> ? R : unknown;
